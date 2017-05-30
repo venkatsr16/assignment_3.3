c1c55f2c3d1d4dbc80ea906490f7617aa0331392
venkatsr16 <venkat5494@gmail.com>
2017-05-30 23:56:22 +0530

    Display the details and result of at-least three students.

---
+++ example.java
@@ -0,0 +1,12 @@
+class example {
+ int x; int y;
+ String name;
+ public static void main(String args[]) {
+ example pnt = new example();
+ System.out.println ("pnt is " + pnt.name + " " + pnt.x + " " + pnt.y);
+} }
+
+
+/*
+Output is : pnt is null 0 0
+*/
